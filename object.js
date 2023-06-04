d2706c481ea5ea433dd4c39544ff428c157406d8
Ibanga Sharon <ibsharon19@gmail.com>
2023-06-04 17:50:53 +0100

    feat: add new changes

--- object.js
+++ object.js
@@ -3,6 +3,7 @@ const frontEnd6b2 = [
         firstName: 'sharon',
         surname: 'ibanga',
         DOB: '28-12',
+        age: 23,
         state: 'akwa-ibom',
         Gender: 'female',
         hobbies: ['eating','sleeping', 'playing','coding']
@@ -12,6 +13,7 @@ const frontEnd6b2 = [
         firstName: 'benson',
         surname: 'ibeh',
         DOB: '06-12',
+        age: 25,
         state: 'abia',
         Gender: 'male',
         hobbies: ['photography','coding','football']
@@ -21,6 +23,7 @@ const frontEnd6b2 = [
         firstName: 'kelubia',
         surname: 'urubusi',
         DOB: '03-08',
+        age: 28,
         state: 'delta',
         Gender: 'male',
         hobbies:[ 'eating','drinking','coding'] 
@@ -30,6 +33,7 @@ const frontEnd6b2 = [
         firstName: 'daniel',
         surname: 'ebuka',
         DOB: '04-19',
+        age: 25,
         state: 'anambra',
         Gender: 'male',
         hobbies: ['eating','coding', 'exploring']
@@ -39,6 +43,7 @@ const frontEnd6b2 = [
         firstName: 'amira',
         surname: 'olusesi',
         DOB: '04-24',
+        age: 22,
         state: 'lagos',
         Gender: 'female',
         hobbies: ['eating', 'coding', 'reading']
@@ -48,6 +53,7 @@ const frontEnd6b2 = [
         firstName: 'gideon',
         surname: 'owadara',
         DOB: '05-17',
+        age: 25,
         state: 'ondo',
         Gender: 'male',
         hobbies: ['coding','reading', 'exploring']
@@ -57,6 +63,7 @@ const frontEnd6b2 = [
         firstName: 'damola',
         surname: 'gbenga',
         DOB: '11-18',
+        age: 24,
         state: 'ekiti',
         Gender: 'male',
         hobbies: ['coding','designing','exploring']
@@ -66,6 +73,7 @@ const frontEnd6b2 = [
         firstName: 'tharewa',
         surname: 'umar',
         DOB: '04-16',
+        age: 18,
         state: 'jigawa',
         Gender: 'female',
         hobbies: ['eating','sleeping','exploring']
@@ -75,6 +83,7 @@ const frontEnd6b2 = [
         firstName: 'samiat',
         surname: 'usman',
         DOB: '04-21',
+        age: 29,
         state: 'kwara',
         Gender: 'female',
         hobbies: ['eating','coding','exploring']
@@ -84,6 +93,7 @@ const frontEnd6b2 = [
         firstName: 'stephen',
         surname: 'momoh',
         DOB: '06-12',
+        age: 24,
         state: 'edo',
         Gender: 'male',
         hobbies: ['coding', 'designing','exploring']
@@ -92,6 +102,7 @@ const frontEnd6b2 = [
         firstName: 'chidinma',
         surname: 'nwaneti',
         DOB: '07-14',
+        age: 23,
         state: 'anambra',
         Gender: 'female',
         hobbies: ['eating','coding', 'exploring']
@@ -101,9 +112,25 @@ const frontEnd6b2 = [
         firstName: 'shayo',
         surname: 'nwaneti',
         DOB: '07-14',
+        age: 23,
         state: 'anambra',
         Gender: 'female',
         hobbies: ['eating','sleeping', 'exploring']
     },
     
 ]
+// const persons = frontEnd6b2.map(person => {
+//     return person.firstName
+// })
+// console.log(persons)
+
+// const Gender = frontEnd6b2.filter(person => {
+//     return person.Gender === 'female'  
+// })
+//  console.log(Gender)
+
+const totalAge = frontEnd6b2.reduce((total, person) => {
+    return total + person.age;
+}, 0)
+
+console.log(totalAge);
